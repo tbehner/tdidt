d77b1c8a681e10870a112cd54175dd68c8eb43ea
tbehner <behner@cs.uni-bonn.de>
2014-11-06 08:26:28 +0100

    At least no error messages anymore
    It is questionable if the computation of TDIDT is correct

--- tdidt.py
+++ tdidt.py
@@ -49,12 +49,17 @@ def get_information_gain(ppos, pneg, npos, nneg):
     """
     compute the information gain given with the four parameters
     """
+#    print("Ppos: {}, Pneg: {}, Npos: {}, Nneg: {}".format(ppos,pneg,npos,nneg))
     total = float(ppos + pneg + npos + nneg)
     p_total = float(ppos + pneg)
     n_total = float(npos + nneg)
+    if total == 0:
+        return 0
     information_gain = entropy((ppos+npos)/total,(pneg + nneg)/total)
-    information_gain -= p_total/total * entropy(ppos/p_total,pneg/p_total)
-    information_gain -= n_total/total * entropy(npos/n_total,nneg/n_total)
+    if p_total > 0:
+        information_gain -= p_total/total * entropy(ppos/p_total,pneg/p_total)
+    if n_total > 0:
+        information_gain -= n_total/total * entropy(npos/n_total,nneg/n_total)
     return information_gain
 
 def entropy(p,n):
@@ -103,15 +108,18 @@ class ExampleSet:
         for line in input_file:
            example_list = list(map(lambda s : s.rstrip() , line.split(',')))
             # FIXME IOOUUUUU there has to be a way which is prettier than this
-            example_hash = dict(zip(sorted(self.attributes),example_list))
-            for key in example_hash.keys(): # convert all numerical values from str to float
-                if self.attributes[key] == 'n':
-                    example_hash[key] = float(example_hash[key])
-            self.examples.append(Example(example_hash,example_list[-1] == "yes"))
-            if example_list[-1] =="yes":
-                self.positives += 1
-            else:
-                self.negatives += 1
+           example_hash = dict(zip(sorted(self.attributes),example_list))
+           for key in example_hash.keys(): 
+               if self.attributes[key] == 'n':# convert all numerical values from str to float
+                   example_hash[key] = float(example_hash[key])
+               elif self.attributes[key] == 'b': # and all boolean values from str to boolean
+                   example_hash[key] = (example_hash[key] == "yes")
+
+           self.examples.append(Example(example_hash,example_list[-1] == "yes"))
+           if example_list[-1] =="yes":
+               self.positives += 1
+           else:
+               self.negatives += 1
 
         self.entropy = entropy(float(self.positives)/len(self.examples), float(self.negatives)/len(self.examples))
 
@@ -122,29 +130,32 @@ class ExampleSet:
         """
         max_information_gain = None
         splitting_test = None
+        split_value = None
         passing_examples = [0,0]
         failing_examples = [self.positives,self.negatives]
 
         # sort the examples according to the values of the attribute
         self.examples.sort(key=lambda e : e.attribute_hash[attribute])
         for i_idx in range(len(self.examples)-1):
-            if self.examples[i_idx].outcome == self.examples[i_idx+1].outcome:              # update distribution of outcomes
-                if self.examples[i_idx].outcome:
-                    failing_examples[0] -= 1
-                    passing_examples[0] += 1
-                else:
-                    failing_examples[1] -= 1
-                    passing_examples[1] += 1
+            if self.examples[i_idx].outcome:
+                failing_examples[0] -= 1
+                passing_examples[0] += 1
             else:
+                failing_examples[1] -= 1
+                passing_examples[1] += 1
+            if not self.examples[i_idx].outcome == self.examples[i_idx+1].outcome:              # update distribution of outcomes
                 # information gain
                 current_information_gain = get_information_gain(passing_examples[0],passing_examples[1],failing_examples[0],failing_examples[1])
 
                 if max_information_gain is None or max_information_gain < current_information_gain:
                     max_information_gain = current_information_gain
                     mean = (self.examples[i_idx].attribute_hash[attribute] + self.examples[i_idx+1].attribute_hash[attribute])/2
+                    split_value = mean
                     # FIXME if the code should brake, I should consider this line as the problem
-                    splitting_test = lambda x : x < mean
-        return [max_information_gain,splitting_test]
+                    splitting_test = lambda x : x.attribute_hash[attribute] < split_value
+        if max_information_gain is None:
+            max_information_gain = 0
+        return [max_information_gain,SplittingTest(splitting_test,attribute,"n",split_value)]
 
     def best_split_categorical(self,attribute):
         """
@@ -157,15 +168,14 @@ class ExampleSet:
         information_gain = {}           # information gain for each value of the attribute
 
         # compute the histograms
-        for example in self.examples:   
+        for example in self.examples:
             value = example.attribute_hash[attribute]
-            if value in histograms.keys():
-                if example.outcome:
-                    histograms[value][0] += 1
-                else:
-                    histograms[value][1] += 1
-            else:
+            if not value in list(histograms.keys()):
                 histograms[value] = [0,0]
+            if example.outcome:
+                histograms[value][0] += 1
+            else:
+                histograms[value][1] += 1
 
         # compute the information gain for each attribute
         for value in histograms.keys():
@@ -197,8 +207,8 @@ class ExampleSet:
                 s_histogram[1] += histograms[value][1]
 
         max_information_gain = previous_gain
-        splitting_test = lambda x : x in s
-        return [max_information_gain,splitting_test]
+        splitting_test = lambda x : x.attribute_hash[attribute] in s
+        return [max_information_gain,SplittingTest(splitting_test,attribute,"c",s)]
 
 
     def split_boolean(self,attribute):
@@ -208,7 +218,7 @@ class ExampleSet:
         passing = [0,0]
         failing = [self.positives,self.negatives]
         for example in self.examples:
-            if example.example_hash[attribute]:
+            if example.attribute_hash[attribute]:
                 if example.outcome:
                     passing[0] += 1
                 else:
@@ -216,8 +226,8 @@ class ExampleSet:
         failing[0] -= passing[0]
         failing[1] -= passing[1]
         information_gain = get_information_gain(passing[0],passing[1],failing[0],failing[1])
-        test = lambda x : x
-        return [information_gain,test]
+        test = lambda e : e.attribute_hash[attribute]
+        return [information_gain,SplittingTest(test,attribute,"b",True)]
 
 
     def get_split(self,attribute):
@@ -229,11 +239,11 @@ class ExampleSet:
         max_information_gain = None
         splitting_test       = None
         if self.attributes[attribute] == 'n': # find the best split for numerical attributes
-            [max_information_gain,splitting_test] = best_split_numerical(attribute)     
+            [max_information_gain,splitting_test] = self.best_split_numerical(attribute)
         elif self.attributes[attribute] == 'c': # find the best split for categorical attributes
-            [max_information_gain,splitting_test] = best_split_categorical(attribute)     
+            [max_information_gain,splitting_test] = self.best_split_categorical(attribute)
         else: # there is only boolean values left which have only one possible splitting test
-            [max_information_gain,splitting_test] = split_boolean(attribute)
+            [max_information_gain,splitting_test] = self.split_boolean(attribute)
         return [max_information_gain,splitting_test]
 
 
@@ -251,27 +261,30 @@ class ExampleSet:
         return test_examples
 
     def split(self,test):
-        succeeding_example_set = []
-        failing_example_set = []
+        succeeding_example_set = ExampleSet()
+        failing_example_set = ExampleSet()
 
         for example in self.examples:
             if test(example):
-                succeeding_example_set.examples.push(example)
+                succeeding_example_set.examples.append(example)
                 if example.outcome:
                     succeeding_example_set.positives += 1
                 else:
                     succeeding_example_set.negatives += 1
             else:
-                failing_example_set.examples.push(example)
+                failing_example_set.examples.append(example)
                 if example.outcome:
                     failing_example_set.positives += 1
                 else:
                     failing_example_set.negatives += 1
+
+        succeeding_example_set.attributes = self.attributes
+        failing_example_set.attributes = self.attributes
         return [succeeding_example_set, failing_example_set]
 
 
 class TDIDTNode:
-    def __init__(self,example_set,parent_idx=None,left_child_idx=None,right_child_idx):
+    def __init__(self,example_set,parent_idx=None,left_child_idx=None,right_child_idx=None):
         self.example_set     = example_set
         self.parent_idx      = parent_idx
         self.left_child_idx  = left_child_idx
@@ -296,27 +309,38 @@ def TDIDT(node_list,attribute_list,current_node_idx):
     if current_node.example_set.positives == 0 and current_node.example_set.negatives > 0:
         current_node.leaf    = True
         current_node.outcome = False
+        print("Perfectly classified False")
+        return
 
     if current_node.example_set.positives > 0 and current_node.example_set.negatives == 0:
         current_node.leaf    = True
         current_node.outcome = True
+        print("Perfectly classified True")
+        return
 
     # if no attriubute is left to classify the data we considere
     # the node to be classified with the majorit of outcomes
     if not attribute_list:
         current_node.leaf = True
         current_node.outcome = (current_node.example_set.positives > current_node.example_set.negatives)
+        print("No attribute left")
+        return
 
     # if there are no examples left, then use the majority of the parent node
     if not current_node.example_set.examples:
+        print("No examples left")
         parent_node = node_list[current_node.parent_idx]
         if parent_node.example_set.positives == 0 and parent_node.example_set.negatives > 0:
             current_node.leaf    = True
             current_node.outcome = False
+            print("Classified with False")
+            return
 
         if parent_node.example_set.positives > 0 and parent_node.example_set.negatives == 0:
             current_node.leaf    = True
             current_node.outcome = True
+            print("Classified with True")
+            return
 
     # now there is certenly something left to be classified
     # calculate the information gain of every attribute
@@ -324,8 +348,8 @@ def TDIDT(node_list,attribute_list,current_node_idx):
     splitting_test       = None
     for attribute in attribute_list:
         information_gain , test = current_node.example_set.get_split(attribute)
-        if information_gain > max_information_gain:
-            max_information_gain = information
+        if max_information_gain is None or information_gain > max_information_gain:
+            max_information_gain = information_gain
             splitting_test       = test
 
     # split the example set with the given test with the maximum information gain
@@ -334,6 +358,7 @@ def TDIDT(node_list,attribute_list,current_node_idx):
     # remove the attribute from the list of possible attributes
     next_attribute_list = copy.deepcopy(attribute_list)
     next_attribute_list.remove(test.attribute)
+    print("Split with attribute: {}".format(test.attribute))
 
     # make decision tree nodes for each new example set
     left_node = TDIDTNode(succeeding_example_set,current_node_idx)
@@ -341,10 +366,15 @@ def TDIDT(node_list,attribute_list,current_node_idx):
 
     left_node_idx = len(node_list)
     right_node_idx = len(node_list)+1
-    node_list.push(left_node)
-    node_list.push(right_node)
+    node_list.append(left_node)
+    node_list.append(right_node)
 
     # and invoke TDIDT on each node
     TDIDT(node_list,next_attribute_list,left_node_idx)
     TDIDT(node_list,next_attribute_list,right_node_idx)
 
+e = ExampleSet()
+e.initialize_from_file(sys.argv[1])
+node_list = [TDIDTNode(e)]
+attribute_list = list(e.attributes.keys())
+TDIDT(node_list,attribute_list,0)
